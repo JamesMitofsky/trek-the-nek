86244c4cb908424dd7d5fc5effaa47d8ba9baee8
James Tedesco <jamestedesco802@gmail.com>
2020-09-23 09:01:39 -0400

    Initial commit
    still need to add sal.js

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+  
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("assets");
+
+
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/sal.js/dist/sal.js": "assets/scripts/sal.js",
+        "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css",
+    });
+
+    return {
+
+        dir: {
+            // ⚠️ These values are both relative to your input directory.
+            includes: "assets",
+            layouts: "_layouts"
+        }
+
+    }
+};
